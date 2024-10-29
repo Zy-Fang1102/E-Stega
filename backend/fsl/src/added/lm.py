49b069502ccb4037b47312ea1e697ed24f1dd357
PKQZPC <980142459@qq.com>
2024-10-29 12:43:34 +0800

    禁止模型选择

--- backend/fsl/src/added/lm.py
+++ backend/fsl/src/added/lm.py
@@ -56,10 +56,7 @@ class Old_LM(nn.Module):
 		self.embedding = nn.Embedding(vocab_size, embed_size)
 		if cell == 'rnn':
 			self.rnn = nn.RNN(embed_size, hidden_dim, num_layers, dropout=dropout_rates)
-		elif cell == 'gru':
-			self.rnn = nn.GRU(embed_size, hidden_dim, num_layers, dropout=dropout_rates)
-		elif cell == 'lstm':
-			self.rnn = nn.LSTM(embed_size, hidden_dim, num_layers, dropout=dropout_rates)
+
 		else:
 			raise Exception('no such rnn cell')
 
@@ -70,7 +67,8 @@ class Old_LM(nn.Module):
 		x = x.long()
 		_ = self.embedding(x)
 		_ = _.permute(1, 0, 2)
-
+		h_all, __ = self.rnn(_)
+		h_all = h_all.permute(1, 0, 2)
 		_ = self.output_layer(h_all)
 		_ = self.log_softmax(_)
 		return _
